fa16b648b9002b0d9c8c0c3b5363d4d1d226d172
diegomr949 <diegomr949@gmail.com>
2026-06-23 13:42:33 -0300

    mejora de mail

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 const CONFIG = {
   // URL generada al publicar la Web App en Apps Script.
   // Termina en /exec (no en /dev)
-  API_URL: 'https://script.google.com/macros/s/AKfycbz62oO9Rj1umN-syrHGe8AP6cawZPXwAAYbDiCK8WDUFDThjdBPA9rL7zzsINQGdebJ/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwONSa6X17_u0onaTr5297gJpysdljxB3i4RVmTFnoCXHklkYGIfsBSCrodCRTOUUCZ/exec',
 
   // Nombre que aparece en el header de la app
   ORG_NAME: 'Mi Organización',
