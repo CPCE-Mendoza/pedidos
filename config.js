81f60400afff0cfabc149df352d74a17fa7c9982
diegomr949 <diegomr949@gmail.com>
2026-07-24 17:07:08 -0300

    admin dos areas

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 // ─────────────────────────────────────────────────────────────
 //  config.js — Configuración del Frontend
-//  Editar estos valores antes de publicar en GitHub Pages.
+
 // ─────────────────────────────────────────────────────────────
 
 const CONFIG = {
   // URL generada al publicar la Web App en Apps Script.
   // Termina en /exec (no en /dev)
-  API_URL: 'https://script.google.com/macros/s/AKfycbzDDbYfSbsdLqwyAQiUT54ZEkbWRNW8Rdt8E9MO33JMMuShA6ma13ebH4WYcOwqW-KM/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzONHyuWzjc3U8bFXpZuDmcinHSgHNS8BDMy8eLlcgMnGmHkw_fekzXRVWiGE_qRG3g/exec',
 
   // Nombre que aparece en el header de la app
   ORG_NAME: 'CPCE Mendoza',
 
   // 👇 CONFIGURACIÓN DE GOOGLE OAUTH 👇
-  // Reemplazá el texto entre comillas con el ID de cliente real que te dio Google Cloud
-  GOOGLE_CLIENT_ID: '393097089260-i83spq9s90vecj3rqp031ogg3381h7mc.apps.googleusercontent.com', 
-  
+  GOOGLE_CLIENT_ID: '393097089260-i83spq9s90vecj3rqp031ogg3381h7mc.apps.googleusercontent.com',
+
   // Dominio corporativo permitido (bloquea accesos externos)
   DOMINIO: 'cpcemza.org.ar',
 
