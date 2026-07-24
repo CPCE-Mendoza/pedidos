fbc1307e329a06b940bdb3e34bcee5cdf3c2d90d
diegomr949 <diegomr949@gmail.com>
2026-07-24 12:35:44 -0300

    mejora pdf

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 const CONFIG = {
   // URL generada al publicar la Web App en Apps Script.
   // Termina en /exec (no en /dev)
-  API_URL: 'https://script.google.com/macros/s/AKfycbxJPXje4KaxOmUME8Mrb8lQgs9NCu-a3vvE8jV70grS2QjN64rSyzy2R2wRCSCZo6u7/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxK70KtWfeMmpGuxEpwdf_etzJzSMZX-io3L2VEMn5mCRHFtybsnucRasa4_EVHfd_a/exec',
 
   // Nombre que aparece en el header de la app
   ORG_NAME: 'Mi Organización',
