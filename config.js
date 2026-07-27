50385ade273bead6810ab232be4d17f4c76dba60
diegomr949 <diegomr949@gmail.com>
2026-07-27 12:35:24 -0300

    config emails

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 const CONFIG = {
   // URL generada al publicar la Web App en Apps Script.
   // Termina en /exec (no en /dev)
-  API_URL: 'https://script.google.com/macros/s/AKfycby9RqA6Fw6g_tktSdvjBhENyzaHR_YXI91hUjBbAYUF4wiDYO6pg7AqEZLPR4HXUaxg/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbyzu_HnmI8d6hx_-5ZWq-JZ67zzym0Txp7ApMQsF86DoIlR8KhVg_DskTQWxOfV57D_/exec',
 
   // Nombre que aparece en el header de la app
   ORG_NAME: 'CPCE Mendoza',
